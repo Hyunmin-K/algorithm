54754b0c382458094ff9e1e0461fc29c2afc5dab
Hyunmin-K <79971659+Hyunmin-K@users.noreply.github.com>
2023-01-26 11:54:12 +0900

    [Silver V] Title: 카드1, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.py
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -1,8 +1,8 @@
 n = int(input())
 num_list = list(range(1,n+1))
-for i in range(n-1):
-    print(num_list[0],end=' ')
-    num_list.remove(num_list[0])
-    num_list.append(num_list[0])
-    num_list.remove(num_list[0])
-print(num_list[0])
+ans_list = []
+while len(num_list) > 1:
+    ans_list.append(num_list.pop(0))
+    # num_list.pop(0)
+    num_list.append(num_list.pop(0))
+print(*ans_list, num_list[0])
